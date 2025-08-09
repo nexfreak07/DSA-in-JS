f4b5d53d70fcaf4f3aa422f44cef1bdd87ee41d3
Akash Dasgupta <akash.dasgupta.jobs@gmail.com>
2025-08-10 03:20:49 +0530

    Day 6 DSA

---
+++ Dynamic Programming/n-th-tribonacci-number.js
@@ -0,0 +1,39 @@
+// The Tribonacci sequence Tn is defined as follows: 
+
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+// Given n, return the value of Tn.
+
+ 
+
+// Example 1:
+
+// Input: n = 4
+// Output: 4
+// Explanation:
+// T_3 = 0 + 1 + 1 = 2
+// T_4 = 1 + 1 + 2 = 4
+// Example 2:
+
+// Input: n = 25
+// Output: 1389537
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+let cache = {};
+var tribonacci = function(n) {
+    if(cache[n]){
+        return cache[n];
+    }
+    if(n==0){
+        return cache[n] = 0
+    }
+    if(n === 1 || n == 2){
+        return cache[n] = 1
+    }
+    return cache[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+    
+};
